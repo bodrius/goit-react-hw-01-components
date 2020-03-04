8d1f17dba8a65bd8429d825b87020e4c878aaf3f
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-03-04 17:10:00 +0200

    final version

--- my-homework-1/src/components/App.js
+++ my-homework-1/src/components/App.js
@@ -1,13 +1,9 @@
-import React from 'react'
-import Profile from './profile/Profile';
-import user from '../user.json';
-
+import React from "react";
+import Profile from "./profile/Profile";
+import user from "../user.json";
 
 const App = () => {
-    
-    return (
-       <Profile user={user}/>
-    );
-}
+  return <Profile user={user} />;
+};
 
 export default App;
--- my-homework-1/src/components/profile/Profile.js
+++ my-homework-1/src/components/profile/Profile.js
@@ -1,41 +1,33 @@
-import React from 'react'
-import css from './profile.module.css';
+import React from "react";
+import css from "./profile.module.css";
 // import user from '../../user.json';
 
+const Profile = ({ user: { name, tag, location, avatar, stats } }) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="user avatar" className={css.avatar} />
+        <p className={css.name}>{name}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
 
-
-const Profile = ({user:{name, tag, location,avatar, stats}}) => {
-    return (
-
-<div className={css.profile}>
-  <div className={css.description}>
-    <img
-      src={avatar}
-      alt="user avatar"
-      className={css.avatar}
-    />
-    <p className={css.name}>{name}</p>
-<p className="tag">@{tag}</p>
-<p className="location">{location}</p>
-  </div>
-
-  <ul className={css.stats}>
-    <li className={css.choise}>
-      <span className={css.label}>Followers</span>
-<span className={css.quantity}>{stats.followers}</span>
-    </li>
-    <li className={css.choise}>
-      <span className={css.label}>Views</span>
-      <span className={css.quantity}>{stats.views}</span>
-    </li>
-    <li className={css.choise}>
-      <span className={css.label}>Likes</span>
-      <span className={css.quantity}>{stats.likes}</span>
-    </li>
-  </ul>
-</div>
-
-    );
-}
+      <ul className={css.stats}>
+        <li className={css.choise}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li className={css.choise}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li className={css.choise}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Profile;
