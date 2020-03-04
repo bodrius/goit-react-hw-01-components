b31dee5c5e6e4c4ee6d80545d8fae9a38e4479d2
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-03-04 17:07:02 +0200

    create first task

---
+++ my-homework-1/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Profile from './profile/Profile';
+import user from '../user.json';
+
+
+const App = () => {
+    
+    return (
+       <Profile user={user}/>
+    );
+}
+
+export default App;
