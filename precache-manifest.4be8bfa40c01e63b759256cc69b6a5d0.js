ad1edbd4055812c47971ed6c39dccd529b597bd7
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-03-05 10:14:10 +0200

    Updates

---
+++ precache-manifest.4be8bfa40c01e63b759256cc69b6a5d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f61d2c2f2b6897efb524410d76ac998",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "e418a9f9b0642abf09e5",
+    "url": "/goit-react-hw-01-components/static/css/main.d85ec14c.chunk.css"
+  },
+  {
+    "revision": "eaa169cc2c3530394fe1",
+    "url": "/goit-react-hw-01-components/static/js/2.2326d59c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.2326d59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e418a9f9b0642abf09e5",
+    "url": "/goit-react-hw-01-components/static/js/main.4e2e2d4f.chunk.js"
+  },
+  {
+    "revision": "bf34eae1e97eed4129a6",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.91334821.js"
+  }
+]);
